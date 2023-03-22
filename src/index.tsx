843e9bb752f50ae7288bdb7701f98195fc40d1fe
ninja0911 <araisora30@gmail.com>
2023-03-22 13:13:46 -0700

    installed apollo/client, updated index.tsx and generated server.

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,28 @@ import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+}) 
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
